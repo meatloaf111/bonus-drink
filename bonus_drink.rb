9a68702cd52c62d5236e6d0593a0f1705e374614
meatloaf111 <meatloaf111jp@gmail.com>
2018-03-01 21:18:53 +0900

    回答作成

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+   amount + bonus_count_for(amount)
   end
+   
+   def self.bonus_count_for(amount)
+    bonus = 0
+    if(amount) >= 3
+	amount -= 3
+        bonus +=1
+        bonus = bonus + (amount / 2) if amount > 0
+    end
+    bonus
+    end
 end
